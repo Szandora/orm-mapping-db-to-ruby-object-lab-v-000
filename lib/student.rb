1fb5350257d6c0a9b93c14eed1b8cb97fde8690f
Sheila Wittman <szandora789@gmail.com>
2018-06-28 20:11:49 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,7 @@
 require 'pry'
  class Student
    attr_accessor :id, :name, :grade
- 
+
    def self.new_from_db(row)
      # create a new Student object given a row from the database
     new_student = self.new
@@ -10,7 +10,7 @@ require 'pry'
     new_student.grade = row[2]
     new_student
    end
- 
+
    def self.all
      # retrieve all the rows from the "Students" database
      # remember each row should be a new instance of the Student class
@@ -73,7 +73,7 @@ require 'pry'
       self.new_from_db(row)
     end
    end
- 
+
    def self.find_by_name(name)
      # find the student in the database given a name
      # return a new instance of the Student class
@@ -86,18 +86,18 @@ require 'pry'
       self.new_from_db(student)
     end.first
    end
-  
+
 
    def save
      sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       INSERT INTO students (name, grade)
        VALUES (?, ?)
      SQL
- 
+
      DB[:conn].execute(sql, self.name, self.grade)
    end
-  
+
 
    def self.create_table
      sql = <<-SQL
@@ -107,10 +107,10 @@ require 'pry'
        grade TEXT
      )
      SQL
- 
+
      DB[:conn].execute(sql)
    end
- 
+
    def self.drop_table
      sql = "DROP TABLE IF EXISTS students"
      DB[:conn].execute(sql)
